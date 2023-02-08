6f89879bb56f5e81f4f5d87f43a71d91f3d68f2b
otash8110 <otash_baxadirov@mail.ru>
2023-02-08 22:19:19 +0500

    Add updateOneUser command

---
+++ FinalProject/FinalProject.Application/Users/Commands/UpdateOneUser/UpdateOneUserCommand.cs
@@ -0,0 +1,20 @@
+﻿using FinalProject.Application.Common.Interfaces;
+using MediatR;
+
+namespace FinalProject.Application.Users.Commands.UpdateOneUser
+{
+    public record UpdateOneUserCommand(string name, string surename, string companyName) : IRequest;
+
+    public class UpdateOneUserCommandHandler : IRequestHandler<UpdateOneUserCommand>
+    {
+        private readonly IUserService userService;
+        public UpdateOneUserCommandHandler(IUserService userService)
+        {
+            this.userService = userService;
+        }
+        public Task<Unit> Handle(UpdateOneUserCommand request, CancellationToken cancellationToken)
+        {
+            return Unit.Value;
+        }
+    }
+}
