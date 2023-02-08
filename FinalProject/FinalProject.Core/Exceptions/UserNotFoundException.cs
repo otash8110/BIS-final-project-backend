5e0018bbcad1856df0fe553110ddaf10adbbde90
otash8110 <otash_baxadirov@mail.ru>
2023-02-08 18:08:26 +0500

    add UserNotFoundException

---
+++ FinalProject/FinalProject.Core/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace FinalProject.Core.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException(string email) : base($"User with email {email} not found") { }
+    }
+}
