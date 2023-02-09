53c7d0a0dd3f4dff9dc8e7c94eb8ad85a12496c8
otash8110 <otash_baxadirov@mail.ru>
2023-02-09 14:05:40 +0500

    CurrentUserService added and implemented

---
+++ FinalProject/FinalProject.API/Services/CurrentUserService.cs
@@ -0,0 +1,16 @@
+﻿using FinalProject.Application.Common.Interfaces;
+using System.Security.Claims;
+
+namespace FinalProject.API.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public string UserId => httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
---
+++ FinalProject/FinalProject.Application/Common/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,7 @@
+﻿namespace FinalProject.Application.Common.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        string UserId { get; }
+    }
+}
