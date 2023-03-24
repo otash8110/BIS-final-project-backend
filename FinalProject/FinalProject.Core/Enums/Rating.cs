185d995d39a2dc4d007e10485c8e3f26efc47948
otash8110 <otash_baxadirov@mail.ru>
2023-03-24 13:31:47 +0500

    Add Rating enum

---
+++ FinalProject/FinalProject.Core/Enums/Rating.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalProject.Core.Enums
+{
+    public enum Rating
+    {
+        VeryBad = 1,
+        Bad = 2,
+        NotBad = 3,
+        Good = 4,
+        VeryGood = 5,
+    }
+}
