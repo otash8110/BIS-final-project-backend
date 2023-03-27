c9c6856f9253357be2dcb0ff9defc77cda2e66ee
otash8110 <otash_baxadirov@mail.ru>
2023-03-27 17:27:11 +0500

    Fully implement GetProductsQuery

--- FinalProject/FinalProject.Application/Products/Queries/GetProducts/GetProductsQuery.cs
+++ FinalProject/FinalProject.Application/Products/Queries/GetProducts/GetProductsQuery.cs
@@ -1,23 +1,28 @@
-﻿using FinalProject.Application.Common.Interfaces;
+﻿using AutoMapper;
+using FinalProject.Application.Common.Interfaces;
 using FinalProject.Core.Entities;
 using MediatR;
 
 namespace FinalProject.Application.Products.Queries.GetProducts
 {
-    public record GetProductsQuery(string userId) : IRequest<ProductDTO>;
+    public record GetProductsQuery(string userId) : IRequest<List<ProductDTO>>;
 
-    public class GetProductQueryHandler : IRequestHandler<GetProductsQuery, ProductDTO>
+    public class GetProductQueryHandler : IRequestHandler<GetProductsQuery, List<ProductDTO>>
     {
         private readonly IBaseRepository<Product> productRepository;
+        private readonly IMapper mapper;
 
-        public GetProductQueryHandler(IBaseRepository<Product> productRepository)
+        public GetProductQueryHandler(IBaseRepository<Product> productRepository, IMapper mapper)
         {
             this.productRepository = productRepository;
+            this.mapper = mapper;
         }
 
-        public async Task<ProductDTO> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        public async Task<List<ProductDTO>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
         {
-            return await productRepository.
+            var result = await productRepository.GetByFilter(p => p.UserId == request.userId);
+
+            return mapper.Map<List<ProductDTO>>(result);
         }
     }
 }
