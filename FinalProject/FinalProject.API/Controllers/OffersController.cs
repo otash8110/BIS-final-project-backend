93019573831b2443f89e59f93bde5cc6776fffe9
otash8110 <otash_baxadirov@mail.ru>
2023-04-24 21:14:57 +0500

    Add offers controller

--- FinalProject/FinalProject.API/Controllers/OffersController.cs
+++ FinalProject/FinalProject.API/Controllers/OffersController.cs
@@ -1,4 +1,5 @@
-﻿using FinalProject.Core.Enums;
+﻿using FinalProject.Application.Common.Interfaces;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,7 +10,23 @@ namespace FinalProject.API.Controllers
     [Authorize(Roles = "Distributor,Admin", Policy = "IsRegistrationApproved")]
     public class OffersController : Controller
     {
-        public IActionResult Index()
+        private readonly IMediator mediator;
+        private readonly ICurrentUserService currentUserService;
+
+        public OffersController(IMediator mediator, ICurrentUserService currentUserService)
+        {
+            this.mediator = mediator;
+            this.currentUserService = currentUserService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateOffer()
+        {
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOffer()
         {
             return View();
         }
